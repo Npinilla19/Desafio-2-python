c31abf9c9eca991c673ad551c00381fcb839495c
Nicole Pinilla <npinilla89@gmail.com>
2024-07-25 18:34:41 -0400

    Se agregan las clasificaciones OMS

--- imc.py
+++ imc.py
@@ -1,5 +1,4 @@
 # agrega libreria 
-from cgi import print_directory
 import math
 
 # solicitar datos a usuario
@@ -18,3 +17,23 @@ IMC = Peso / math.pow(Altura1, 2)
 # Mostrar Resultado IMC 
 
 print(f"Su IMC es {round(IMC,2)}")
+
+if IMC < 18.5 :
+    print("Su Clasificacion segun OMS es BAJO PESO")
+
+if IMC > 18.5 < 25 :
+    print("Su clasificacion segun OMS es ADECUADO")
+
+if IMC > 25 < 30 :
+    print("Su clasificacion segun OMS es SOBREPESO")
+
+if IMC > 30 < 35 :
+    print("Su clasificacion segun OMS es OBESIDAD GRADO 1")
+
+if IMC > 35 < 40 :
+    print("Su clasificacion segun OMS es OBESIDAD GRADO 2")
+
+if IMC > 40 :
+    print("Su clasificacion segun OMS es OBESIDAD GRADO 3")
+
+
